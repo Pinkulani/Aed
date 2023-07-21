4d3be9d0853b8d1ac4e74b63900a3806cda13d86
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-07-21 04:31:14 +0200

    Update Emilion.py

--- Emilion.py
+++ Emilion.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter import ttk
-from Helper import Helper
 
 # Setup
 Height = "1000"
@@ -14,12 +13,11 @@ Style = ttk.Style()
 Style.theme_use("clam")
 
 # Icons
-
-Icons = ("Home", "Storage", "Calculator", "Test")
+Icons = ("Home", "Storage", "Calculator", "Timer", "Notes", "Contacts", "Chat", "Terminal", "Test")
 
 # Windows
 Window = ttk.PanedWindow(Emilion, orient=VERTICAL)
-Window.place(x = 0, y = 0, width = Widht, height = Height)
+Window.place(x = 0 , y = 0, width = Widht, height = Height)
 
 # Frames
 Frame = Frame(Window, relief = "ridge")
@@ -30,8 +28,9 @@ Distance = 0
 ButtonIcons = []
 for i in range(len(Icons)):
     ButtonIcons.append(Button(Frame, text = Icons[i], relief = "ridge", font = ("", 10)))
-    ButtonIcons[i].place(x = Distance, y = 0, width = 32, height = 32)
+    ButtonIcons[i].place(x = 0, y = Distance, width = 64, height = 32)
     Distance += 32
 
 # Loop
-mainloop()
+Emilion.minsize(500, 500)
+Emilion.mainloop()

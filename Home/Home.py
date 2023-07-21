0128d637b264329a9e4b2ab11b5da792d3ec7b3c
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-07-21 20:14:58 +0200

    Update Home.py

--- Home/Home.py
+++ Home/Home.py
@@ -1,6 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+
 class Home(object):
     def __init__(self):
-        x = 1
+        pass
 
-    def lel(nm):
-        print("lol")
+    def test(self):
+        Label = LabelFrame(text = "Test Frame")
+        Label.place(x = 500, y = 500, width = 400, height = 400)
+        
+

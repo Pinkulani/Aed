07d7dd37e01aa1d735eb39a1d6c717ae0dbc6809
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-07-21 20:13:46 +0200

    Update Helper.py

--- Helper.py
+++ Helper.py
@@ -1,18 +1,37 @@
 from tkinter import *
+from tkinter import messagebox
 
 from Home.Home import Home
 
 class Helper(object):
     def __init__(self):
         self.Number = -1
-    
     def Run(self, Number):
-        match Number:
+        match Number: # Apps
             case 0:
                 print("Home")  
             case 1:
                 print("Storage")
-            case _:
-                print("File not found") 
+            case 2:
+                print("Calculator") 
+            case 3:
+                print("Timer") 
+            case 4:
+                print("Colorpicker") 
+            case 5:
+                print("Notes") 
+            case 6:
+                print("Contacts") 
+            case 7:
+                print("Chat") 
+            case 8:
+                print("Clock")
+            case 9:
+                print("Terminal") 
+            case 10:
+                print("Test")
+                Home.test(self)
 
-h1 = Home()
+            case _: # Error
+                print("File not found")
+                messagebox.showerror("Error", "File was not found.")

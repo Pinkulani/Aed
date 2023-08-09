c9977a93090410439dfff957b6dcdf57d07aa94c
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-09 17:17:38 +0200

    Adding class, functions and color for every button

--- Emilion.py
+++ Emilion.py
@@ -7,8 +7,94 @@ class Home(object):
         pass
 
     def Draw(self):
-        Label = LabelFrame(FrameRight, text = "Test Frame")
+        Label = LabelFrame(FrameRight, text = "Test Frame", bg = "Orange")
         Label.place(x = 10, y = 10, width = 500, height = 500)
+
+class Storage(object):
+    def __init__(self):
+        pass
+
+    def Draw(self):
+        Label = LabelFrame(FrameRight, text = "Test Frame", bg = "Yellow")
+        Label.place(x = 10, y = 10, width = 500, height = 500)
+
+class Calculator(object):
+    def __init__(self):
+        pass
+
+    def Draw(self):
+        Label = LabelFrame(FrameRight, text = "Test Frame", bg = "Green")
+        Label.place(x = 10, y = 10, width = 500, height = 500)
+
+class Timer(object):
+    def __init__(self):
+        pass
+
+    def Draw(self):
+        Label = LabelFrame(FrameRight, text = "Test Frame", bg = "Cyan")
+        Label.place(x = 10, y = 10, width = 500, height = 500)
+
+class Colorpicker(object):
+    def __init__(self):
+        pass
+
+    def Draw(self):
+        Label = LabelFrame(FrameRight, text = "Test Frame", bg = "Blue")
+        Label.place(x = 10, y = 10, width = 500, height = 500)
+
+class Notes(object):
+    def __init__(self):
+        pass
+
+    def Draw(self):
+        Label = LabelFrame(FrameRight, text = "Test Frame", bg = "Purple")
+        Label.place(x = 10, y = 10, width = 500, height = 500)
+
+class Contacts(object):
+    def __init__(self):
+        pass
+
+    def Draw(self):
+        Label = LabelFrame(FrameRight, text = "Test Frame", bg = "Pink")
+        Label.place(x = 10, y = 10, width = 500, height = 500)
+
+class Chat(object):
+    def __init__(self):
+        pass
+
+    def Draw(self):
+        Label = LabelFrame(FrameRight, text = "Test Frame", bg = "Spring Green")
+        Label.place(x = 10, y = 10, width = 500, height = 500)
+
+class Clock(object):
+    def __init__(self):
+        pass
+
+    def Draw(self):
+        Label = LabelFrame(FrameRight, text = "Test Frame", bg = "Yellow")
+        Label.place(x = 10, y = 10, width = 500, height = 500)
+
+class Terminal(object):
+    def __init__(self):
+        pass
+
+    def Draw(self):
+        Label = LabelFrame(FrameRight, text = "Test Frame", bg = "White")
+        Label.place(x = 10, y = 10, width = 500, height = 500)
+
+class Test(object):
+    def __init__(self):
+        pass
+
+    def Draw(self):
+        Label = LabelFrame(FrameRight, text = "Test Frame", bg = "Black")
+        Label.place(x = 10, y = 10, width = 500, height = 500)
+
+class Destructor(object):
+    def __init__(self):
+        pass
+    def Destroy():
+        pass
 
 # Run Number (App) that is listed in Order
 class Helper(object):
@@ -21,24 +107,37 @@ class Helper(object):
                 Home.Draw(self)
             case 1:
                 print("Storage")
+                Storage.Draw(self)
             case 2:
                 print("Calculator") 
+                Calculator.Draw(self)
             case 3:
                 print("Timer") 
+                Timer.Draw(self)
             case 4:
-                print("Colorpicker") 
+                print("Colorpicker")
+                Colorpicker.Draw(self) 
             case 5:
                 print("Notes") 
+                Notes.Draw(self)
             case 6:
                 print("Contacts") 
+                Contacts.Draw(self)
             case 7:
                 print("Chat") 
+                Chat.Draw(self)
             case 8:
                 print("Clock")
+                Clock.Draw(self)
             case 9:
                 print("Terminal") 
+                Terminal.Draw(self)
             case 10:
                 print("Test")
+                Test.Draw(self)
+            case 11:
+                print("Quit")
+                quit()
             case _: # Error
                 # Impossible Error pretty much since I can't implement importing Classes that work with Tkinter dynamically only hardcoded.
                 print("File not found")
@@ -63,14 +162,14 @@ Window = ttk.PanedWindow(Emilion, orient = HORIZONTAL)
 Window.place(x = 0 , y = 0, width = 1000 , height = 1000)
 
 # Frames
-FrameLeft = Frame(Window, bg = "cyan", relief = "ridge") # Blue Window
+FrameLeft = Frame(Window, bg = "Cyan", relief = "ridge") # Blue Window
 Window.add(FrameLeft, weight = 2) # Start position of Slider
 
-FrameRight = Frame(Window, bg = "red", relief = "ridge") # Red Window
+FrameRight = Frame(Window, bg = "Red", relief = "ridge") # Red Window
 Window.add(FrameRight, weight = 30)
 
 # Icons that will appear
-Icons = ("Home", "Storage", "Calculator", "Timer", "Colorpicker", "Notes", "Contacts", "Chat", "Clock", "Terminal", "Test")
+Icons = ("Home", "Storage", "Calculator", "Timer", "Colorpicker", "Notes", "Contacts", "Chat", "Clock", "Terminal", "Test", "Quit")
 
 # Draw Menu
 Distance = 0

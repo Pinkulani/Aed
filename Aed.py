4c8aa2ed6e5394c154cdcf62005aaa1540366859
Emilia <97615383+Pinkulani@users.noreply.github.com>
2025-05-22 22:47:58 +0200

    Made build system for clang++

--- Aed.py
+++ Aed.py
@@ -1,3 +1,27 @@
+# © Pinkulani
+
+import subprocess
+
+Files = []
 with open("Build.aed", "r") as File:
     for Line in File:
-        print(Line.strip())
+        Files.append(Line.strip()) # Removes newline character
+
+First = Files[0] # Save main file
+Name = ""
+for Character in First: # Get program name
+    if Character == ".":
+        break
+    else:
+        Name += Character
+
+Command = "clang++ " + First + " " # Get compiler command and main file ready
+for X in range(len(Files) - 1):
+    Command += "Source/" + Files[X + 1] + " " # Add other files and source directory
+
+Command += "-o " + Name # Add name for compilation
+
+print("Command ran:", Command)
+Result = subprocess.run(Command, shell = True, capture_output = True, text = True)
+print("Output: ", Result.stdout)
+print("Error: ", Result.stderr)

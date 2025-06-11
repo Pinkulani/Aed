bf564ba7f82b330138c273d1b331cc2a2350522d
Emilia <97615383+Pinkulani@users.noreply.github.com>
2025-06-11 10:35:41 +0200

    Updated for Windows support

--- Aed.py
+++ Aed.py
@@ -3,6 +3,10 @@
 import subprocess, platform
 
 print("Building for platform:", platform.system())
+if platform.system() == "Windows":
+    Tool = "cl "
+else:
+    Tool = "clang++ " # clang for Unix because of better support for ARM
 
 Files = []
 with open("Build.aed", "r") as File:
@@ -17,13 +21,17 @@ for Character in First: # Get program name
     else:
         Name += Character
 
-Command = "clang++ " + First + " " # Get compiler command and main file ready
+Command = Tool + First + " " # Get compiler command and main file ready
 for X in range(len(Files) - 1):
     Command += "Source/" + Files[X + 1] + " " # Add other files and source directory
 
 Command += "-o " + Name # Add name for compilation
 
+if platform.system() == "Windows": # Windows needs .bat for call function which is not supported in PowerShell and globals don't really exist
+    Result = subprocess.run(".\Make.bat && "+ Command, shell = True, capture_output = True, text = True)
+else:
+    Result = subprocess.run(Command, shell = True, capture_output = True, text = True)
+    
 print("Command ran:", Command)
-Result = subprocess.run(Command, shell = True, capture_output = True, text = True)
 print("Output: ", Result.stdout)
 print("Error: ", Result.stderr)

df8c6fcba702d3417f0bb5b7a968bc0a40a17ba8
Emilia <97615383+Pinkulani@users.noreply.github.com>
2025-10-06 12:10:43 +0200

    Added custom build function

--- Aed.py
+++ Aed.py
@@ -1,21 +1,29 @@
 # © 2025 Pinkulani
 
-import subprocess, platform, os
+import subprocess, platform, sys, os
+
+BuildCustom = False
 
 print("Aed © Pinkulani")
 
-try:
-    with open("Build.aed", "r") as File: # Save configuration
-        Config = []
-        for Line in File:
-            Config.append(Line.strip()) # Remove newline character
-        Tool = Config[0]
-        Name = Config[1]
-        Extension = Config[2]
-except FileNotFoundError:
-    print("Build file not found.")
+def Version():
+    print("Aed - Experimental")
 
-def GetFiles():
+def GetConfig() -> str:
+    try:
+        with open("Config.aed", "r") as File: # Save configuration
+            Config = []
+            for Line in File:
+                Config.append(Line.strip()) # Remove newline character
+    except PermissionError:
+        print("Insufficient permission.")
+        sys.exit(1)
+    except FileNotFoundError:
+        print("Build file not found.")
+        sys.exit(1)
+    return Config
+
+def GetFiles(Extension: str) -> str: # Automatic
     Files = [] # Source files
     for root, dirs, files in os.walk(os.getcwd()):
         for file in files:
@@ -23,14 +31,86 @@ def GetFiles():
                 Files.append(os.path.join(root, file))
     return Files
 
-print("Building for platform:", platform.system())
-Command = Tool + " "
-Files = GetFiles()
-for X in range(len(Files)):
-    Command += Files[X] + " "
-Command += "-o " + Name
+def GetFilesCustom() -> str: # From text file
+    Files = []
+    try:
+        with open("Custom.aed", "r") as File:
+            for Line in File:
+                Files.append(Line.strip())
+    except PermissionError:
+        print("Insufficient permission.")
+        sys.exit(1)
+    except FileNotFoundError:
+        print("Custom build file not found.")
+        sys.exit(1)
+    return Files
 
-Result = subprocess.run(Command, shell = True, capture_output = True, text = True)
+def Create(): # Creating build folder
+    Path = os.getcwd() + "/Build"
+    try:
+        print("Creating build folder at:", Path)
+        os.mkdir(Path)
+    except PermissionError:
+        print("Insufficient permission.")
+        sys.exit(1)
+    except FileNotFoundError:
+        print("Part of the path doesn't exist.")
+        sys.exit(1)
 
-print("Command ran:", Command)
-print("Error:", Result.stderr)
+def Build():
+    print("Building for platform:", platform.system())
+    Config = GetConfig()
+    Tool = Config[0]
+    Name = Config[1]
+    Extension = Config[2]
+    Optimization = Config[3]
+
+    Command = Tool + " "
+    Command += "-o " + Name
+    Command += " " + Optimization + " "
+
+    if BuildCustom == True:
+        Files = GetFilesCustom()
+    else:
+        Files = GetFiles(Extension)
+    
+    for X in range(len(Files)): # Append files
+            Command += Files[X] + " "
+
+    try: # Change to build directory, building with absolute paths
+        os.chdir(os.getcwd() + "/Build")
+    except PermissionError:
+        print("Insufficient permission.")
+        sys.exit(1)
+    except FileNotFoundError:
+        print("Build directory doesn't exist.")
+        sys.exit(1)
+
+    Result = subprocess.run(Command, shell = True, capture_output = True, text = True)
+
+    print("Command ran:", Command)
+    print("Error:", Result.stderr)
+
+def ActivateCustom():
+    global BuildCustom
+    BuildCustom = True
+
+# argv = ['Aed.py', 'Command']
+if len(sys.argv) == 2:
+    match sys.argv[1].lower():
+        case "build":
+            Build()
+        case "create":
+            Create()
+        case "-v":
+            Version()
+        case "version":
+            Version()
+elif len(sys.argv) == 3:
+    if sys.argv[1].lower() == "build" and sys.argv[2].lower() == "auto":
+        Build()
+    if sys.argv[1].lower() == "build" and sys.argv[2].lower() == "custom":
+        ActivateCustom()
+        Build()
+else:
+    print("No or invalid arguments given.")

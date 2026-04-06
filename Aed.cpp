c6f140faaab2b270497f6c0531d4ad5fd3c5c3d6
Emilia <97615383+Pinkulani@users.noreply.github.com>
2026-04-06 21:03:13 +0200

    Update Aed.cpp

--- Aed.cpp
+++ Aed.cpp
@@ -1,8 +1,27 @@
+// © 2026 Pinkulani
+
 #include <iostream>
+#include <string>
+#include <filesystem>
+#include <fstream>
 
 bool Custom = false;
 
+void Create() {
+    std::filesystem::path Directory = "Build";
+    if (std::filesystem::exists(Directory)) {
+        std::cout << "Yes" << std::endl;
+    } else {
+        std::cout << "Creating a build folder in project..." << std::endl;
+    }
+}
+
+void Config() {
+
+}
+
 int main() {
+    Create();
     std::cout << "Aed" << std::endl;
     return 0;
 }
